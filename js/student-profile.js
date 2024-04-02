4c2db7baf7135698d924340951d23f5bfd6dc747
ralph canlas <rcanlas012003@gmail.com>
2024-04-02 21:18:54 +0800

    bagong notif

--- js/student-profile.js
+++ js/student-profile.js
@@ -1,27 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
   const editProfileButton = document.getElementById("editProfileButton");
   const editProfileModal = document.getElementById("editProfileModal");
-  const closeModal = document.querySelector(".close");
+  const editProfileCloseBtn = document.querySelector(
+    "#editProfileModal .close"
+  );
+  const notificationModal = document.getElementById("notificationModal");
+  const notificationIcon = document.getElementById("notificationIcon");
+  const notificationCloseBtn = document.querySelector(
+    "#notificationModal .notif-close"
+  );
 
-  // Show modal when "Edit Profile" button is clicked
+  // Show edit profile modal when "Edit Profile" button is clicked
   editProfileButton.addEventListener("click", function () {
     editProfileModal.style.display = "block";
   });
 
-  // Close modal when the close button is clicked
-  closeModal.addEventListener("click", function () {
+  // Close edit profile modal when the close button is clicked
+  editProfileCloseBtn.addEventListener("click", function () {
     editProfileModal.style.display = "none";
   });
 
-  // Close modal when clicking outside of it
+  // Close edit profile modal when clicking outside of it
   window.addEventListener("click", function (event) {
     if (event.target === editProfileModal) {
       editProfileModal.style.display = "none";
     }
   });
 
-  // Prevent the modal from closing when clicking inside the modal content
+  // Prevent the edit profile modal from closing when clicking inside the modal content
   editProfileModal.addEventListener("click", function (event) {
     event.stopPropagation();
   });
+
+  // Show notification modal when notification icon is clicked
+  notificationIcon.onclick = function () {
+    notificationModal.style.display = "block";
+  };
+
+  // Close notification modal when the close button is clicked
+  notificationCloseBtn.onclick = function () {
+    notificationModal.style.display = "none";
+  };
+
+  // Close notification modal when clicking outside of it
+  window.onclick = function (event) {
+    if (event.target == notificationModal) {
+      notificationModal.style.display = "none";
+    }
+  };
 });
